8cca86c8001697f5f41e0b6ea81f36647700bd38
shandy <hsujuihsuan@seattleu.edu>
2018-04-21 23:38:22 -0700

    test

--- empdata.js
+++ empdata.js
@@ -2,19 +2,180 @@
   "data": [
     {
       "name": "Amumu",
-      "title": "The sad mummy",
+      "title": "The Sad Mummy",
       "sex": "Male",
       "region": "Shurima",
       "role": "Tank",
-      "ultimate": "Curse of the sad mummy"
+      "ultimate": "Curse of the Sad Mummy"
     },
     {
       "name": "Anivia",
-      "title": "The cryophoenix",
+      "title": "The Cryophoenix",
       "sex": "Female",
       "region": "Freljord",
       "role": "Mage",
       "ultimate": "Glacial Storm"
+    },
+    {
+      "name": "Annie",
+      "title": "The Dark Child",
+      "sex": "Female",
+      "region": "Runeterra",
+      "role": "Mage",
+      "ultimate": "Summon:Tibbers"
+    },
+    {
+      "name": "Blitzcrank",
+      "title": "The Great Steam Golem",
+      "sex": "Male",
+      "region": "Zaun",
+      "role": "Tank",
+      "ultimate": "Static Field"
+    },
+    {
+      "name": "Caitlyn",
+      "title": "The Sheriff Of Piltover",
+      "sex": "Female",
+      "region": "Piltover",
+      "role": "Marksman",
+      "ultimate": "Ace in the Hole"
+    },
+    {
+      "name": "Cassiopeia",
+      "title": "The Serpent's Embrace",
+      "sex": "Female",
+      "region": "Noxus",
+      "role": "Mage",
+      "ultimate": "Petrifying Gaze"
+    },
+    {
+      "name": "Cho'Gath",
+      "title": "The Terror of the void",
+      "sex": "Male",
+      "region": "Void",
+      "role": "Tank",
+      "ultimate": "Feast"
+    },
+    {
+      "name": "Darius",
+      "title": "The Hand Of Noxus",
+      "sex": "Male",
+      "region": "Noxus",
+      "role": "Fighter",
+      "ultimate": "Noxian Guillotine"
+    },
+    {
+      "name": "Diana",
+      "title": "Scorn Of The Moon",
+      "sex": "Female",
+      "region": "Mount Targon",
+      "role": "Fighter",
+      "ultimate": "Lunar Rush"
+    },
+    {
+      "name": "Garen",
+      "title": "The Might Of Demacia",
+      "sex": "Male",
+      "region": "Demacia",
+      "role": "Fighter",
+      "ultimate": "Demacian Justice"
+    },
+    {
+      "name": "Hecarim",
+      "title": "The Shadow Of War",
+      "sex": "Male",
+      "region": "Shadow Isles",
+      "role": "Fighter",
+      "ultimate": "Onslaught of Shadows"
+    },
+    {
+      "name": "Irelia",
+      "title": "The Blade Dance",
+      "sex": "Female",
+      "region": "Ionia",
+      "role": "Fighter",
+      "ultimate": "Vanguard's Edge"
+    },
+    {
+      "name": "Janna",
+      "title": "The Storm's Fury",
+      "sex": "Female",
+      "region": "Zaun",
+      "role": "Support",
+      "ultimate": "Monsoon"
+    },
+    {
+      "name": "Kassadin",
+      "title": "The Void Walker",
+      "sex": "Male",
+      "region": "Void",
+      "role": "Assassin",
+      "ultimate": "Riftwalk"
+    },
+    {
+      "name": "Leona",
+      "title": "The Radiant Dawn",
+      "sex": "Female",
+      "region": "Mount Targon",
+      "role": "Tank",
+      "ultimate": "Solar Flare"
+    },
+    {
+      "name": "Master Yi",
+      "title": "The Wuju Bladesman",
+      "sex": "Male",
+      "region": "Ionia",
+      "role": "Assassin",
+      "ultimate": "Highlander"
+    },
+    {
+      "name": "Nunu",
+      "title": "The Yeti Rider",
+      "sex": "Male",
+      "region": "Freljord",
+      "role": "Support",
+      "ultimate": "Absolute Zero"
+    },
+    {
+      "name": "Rammus",
+      "title": "The Armordillo",
+      "sex": "Male",
+      "region": "Shurima",
+      "role": "Tank",
+      "ultimate": "Tremors"
+    },
+    {
+      "name": "Rengar",
+      "title": "The Pridestalker",
+      "sex": "Male",
+      "region": "Runeterra",
+      "role": "Assassin",
+      "ultimate": "Thrill of the Hunt"
+    },
+    {
+      "name": "Teemo",
+      "title": "The Swift Scout",
+      "sex": "Male",
+      "region": "Bandle City",
+      "role": "Marksman",
+      "ultimate": "Noxious Trap"
+    },
+    {
+      "name": "Vayne",
+      "title": "The Night Hunter",
+      "sex": "Female",
+      "region": "Demacia",
+      "role": "Marksman",
+      "ultimate": "Final Hour"
+    },
+    {
+      "name": "Zoe",
+      "title": "The Aspect Of Twilight",
+      "sex": "Female",
+      "region": "Mount Targon",
+      "role": "Mage",
+      "ultimate": "Portal Jump"
+  
     }
   ]
 }
